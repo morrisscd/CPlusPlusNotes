cfc1369d0a47b22ea34c4be8785e823a8f163885
Kryten <kryten2q4b@hotmail.co.uk>
2020-06-15 16:16:33 +0100

    First commit

--- notes.cpp
+++ notes.cpp
@@ -16,22 +16,6 @@ using namespace concurrency::streams;       // Asynchronous streams
 using namespace web::json;
 
 
-#include "cpprest/containerstream.h"
-#include "cpprest/filestream.h"
-#include "cpprest/http_client.h"
-#include "cpprest/json.h"
-#include "cpprest/producerconsumerstream.h"
-#include <iostream>
-#include <sstream>
-
-using namespace ::pplx;
-using namespace utility;
-using namespace concurrency::streams;
-
-using namespace web;
-using namespace web::http;
-using namespace web::http::client;
-using namespace web::json;
 
 
 int main()
---
+++ testfile.cpp
@@ -0,0 +1,19 @@
+
+#include "cpprest/containerstream.h"
+#include "cpprest/filestream.h"
+#include "cpprest/http_client.h"
+#include "cpprest/json.h"
+#include "cpprest/producerconsumerstream.h"
+#include <iostream>
+#include <sstream>
+
+using namespace ::pplx;
+using namespace utility;
+using namespace concurrency::streams;
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace web::json;
+
+
