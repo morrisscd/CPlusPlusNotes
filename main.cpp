f099ccd7189138af11057b6faab5d1bc80a16526
Kryten <kryten2q4b@hotmail.co.uk>
2020-06-20 10:22:58 +0100

    Corrected timer code

--- main.cpp
+++ main.cpp
@@ -2,13 +2,30 @@
 //
 #include "pch.h"
 #include "main.h"
+#include "timer.h"
 
 int main()
 {
 
-	int temperature = GetTemperature();
+    Timer t = Timer();
 
-	std::cout << temperature << std::endl;
+    t.setInterval([&]() {
+        std::cout << "Hey.. After each 1s..." << std::endl;
+    }, 1000);
+
+    t.setTimeout([&]() {
+        std::cout << "Hey.. After 5.2s. But I will stop the timer!" << std::endl;
+        t.stop();
+    }, 5200);
+
+
+    std::cout << "I am Timer" << std::endl;
+
+    while (true); // Keep main thread active
+
+	//int temperature = GetTemperature();
+
+	//std::cout << temperature << std::endl;
 
 }
 
--- timer.cpp
+++ timer.cpp
@@ -1,41 +1,4 @@
 
 #include "pch.h"
+#include "timer.h"
 
-class Timer {
-    bool clear = false;
-
-public:
-    template<typename Function>
-    void setTimeout(Function function, int delay);
-
-    template<typename Function>
-    void setInterval(Function function, int interval);
-
-    void stop();
-};
-
-template<typename Function>
-void Timer::setTimeout(Function function, int delay) {
-    this->clear = false;
-    std::thread t([=]() {
-        if (this->clear) return;
-        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-        if (this->clear) return;
-        function();
-    });
-    t.detach();
-}
-
-template<typename Function>
-void Timer::setInterval(Function function, int interval) {
-    this->clear = false;
-    std::thread t([=]() {
-        while (true) {
-            if (this->clear) return;
-            std::this_thread::sleep_for(std::chrono::milliseconds(interval));
-            if (this->clear) return;
-            function();
-        }
-    });
-    t.detach();
-}
